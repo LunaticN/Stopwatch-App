7244d08bb5de0549c36c26e76023f1b15fa2e9b1
BT_1N3_07 <nayaa@nycstudents.net>
2024-10-23 09:32:38 -0400

    timer almost done, need listeners

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.myapplication
 
+import android.media.Image
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var play: ImageButton = findViewById(R.id.play)
+    var pause: ImageButton = findViewById(R.id.pause)
+    var reset: ImageButton = findViewById(R.id.reset)
+    var time: TextView = findViewById(R.id.time)
+    var min: Int = 0
+    var sec: Int = 0
+    var hundSec: Int = 0
+    var running: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        play.setOnClickListener {
+            stopwatch()
         }
+    }
+
+    private fun stopwatch(){
+        val handler: Handler = Handler(Looper.getMainLooper())
+        var runner: Runnable = Runnable { }
+        runner = Runnable {
+            handler.postDelayed(runner, 100)
+            hundSec++
+            if (hundSec == 10){
+                sec++
+                hundSec = 0
+            }
+            if (sec == 60){
+                min++
+                sec = 0
+            }
+        }
+        time.text = String.format("%02d", min) + "." + String.format("%02d", sec) + hundSec.toString()
     }
 }
